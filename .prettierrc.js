6064b1012dafe7e7efeca6d3f147e4c080b674d1
shljy <1759158360@qq.com>
2024-12-05 14:16:15 +0800

    11

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,7 +3,7 @@
 /** @type {import("prettier").Config} */
 export default {
   bracketSpacing: true,
-  singleQuote: false,
+  singleQuote: false, //指定是否使用单引号
   arrowParens: "avoid",
-  trailingComma: "none"
+  trailingComma: "none" //指定是否在对象或数组字面量的最后一个元素后面添加逗号。可选值包括 'es5'（在 ES5 中允许添加行末逗号，也是 Prettier 的默认选项）、'none'（不允许加逗号）和 'all'（在所有可能的位置都加逗号）
 };
