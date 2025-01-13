cc84c9a8610343774635da8bfd933ce3de4bceb8
shljy <1759158360@qq.com>
2025-01-13 10:20:45 +0800

    斐波那契函数

--- src/views/elementUI/table/test/index.vue
+++ src/views/elementUI/table/test/index.vue
@@ -7,3 +7,34 @@ defineOptions({
 });
 </script>
 <style lang="scss" scoped></style>
+
+<!-- <template>
+  <div>等待编写</div>
+</template>
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted } from "vue";
+defineOptions({
+  name: "testPage"
+});
+let timer;
+const sequence = fibonacci();
+onMounted(() => {
+  timer = setInterval(() => {
+    console.log(sequence.next().value);
+  }, 1000);
+});
+
+function* fibonacci() {
+  let current = 0;
+  let next = 1;
+  while (true) {
+    const reset = yield current;
+    [current, next] = [next, next + current];
+    if (reset) {
+      current = 0;
+      next = 1;
+    }
+  }
+}
+</script>
+<style lang="scss" scoped></style> -->
