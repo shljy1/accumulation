eec99df0ed7d086e88b6bf803bfb1d449beb4c64
shljy <1500248024@qq.com>
2025-10-13 14:47:24 +0800

    模型加载优化

--- src/views/three/girl/girlDemo.vue
+++ src/views/three/girl/girlDemo.vue
@@ -9,33 +9,36 @@ const loadingManager = new THREE.LoadingManager(function () {});
 const loader = new ColladaLoader(loadingManager);
 // const axesHelper = new THREE.AxesHelper( 5 );
 // scene.add( axesHelper );
-const archer = await useGLTF("/threeDemo/archer/scene.gltf");
-await loader.load(
-  "/threeDemo/girl/elf.dae",
-  function (collada) {
-    const model = collada.scene;
-    const archerModel = archer.scene;
-    scene.add(model);
-    scene.add(archerModel);
-    model.updateMatrixWorld(true);
-    const box = new THREE.Box3().setFromObject(model);
 
-    // 计算模型中心
-    const center = box.getCenter(new THREE.Vector3());
-    // 将模型移动到相机视野的中心
-    model.position.sub(center);
-    archerModel.position.z = -0.4;
-    archerModel.position.x = -0.8;
-    archerModel.position.y = -0.2;
-    archerModel.rotation.y = 0.2;
-  },
-  function (xhr) {
-    emit("loadProgress", ((xhr.loaded / xhr.total) * 100).toFixed(2) + "%");
-    if (xhr.loaded / xhr.total == 1) {
-      document.querySelector(".loading").remove();
-    }
-  }
-);
+const [archer, collada]: Array<any> = await Promise.all([
+  useGLTF("/threeDemo/archer/scene.gltf"),
+  new Promise((resolve, reject) => {
+    loader.load(
+      "/threeDemo/girl/elf.dae",
+      resolve,
+      function (xhr) {
+        emit("loadProgress", ((xhr.loaded / xhr.total) * 100).toFixed(2) + "%");
+      },
+      reject
+    );
+  })
+]);
+
+const model = collada.scene;
+const archerModel = archer.scene;
+scene.add(model);
+scene.add(archerModel);
+model.updateMatrixWorld(true);
+const box = new THREE.Box3().setFromObject(model);
+
+const center = box.getCenter(new THREE.Vector3());
+model.position.sub(center);
+archerModel.position.z = -0.4;
+archerModel.position.x = -0.8;
+archerModel.position.y = -0.2;
+archerModel.rotation.y = 0.2;
+
+document.querySelector(".loading").remove();
 </script>
 
 <template>
